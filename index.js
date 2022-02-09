c395c98a6426a299bd086e2ab106456fa5bd14b6
ZACCARIA MIR <7004289@cpsd.us>
2022-02-09 12:49:36 -0500

    first time

---
+++ index.js
@@ -0,0 +1,39 @@
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const photoData = require('./data.json');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+
+app.get('/portfolio', (req, res) => {
+
+    const { portfolio } = req.params;
+    res.render('portfolio', {portfolio});
+
+});
+
+app.get('/portfolio/photography', (req, res) => {
+    const { photography } = req.params;
+    const data = photoData[photography];
+    if(data) {
+        res.render('photography', { ...data })
+    }
+
+});
+
+app.get('/portfolio/photo', (req, res) => {
+    res.render('photo')
+
+
+});
+
+
+app.listen(3000, () => {
+    console.log("LISTENING ON PORT 3000")
+});
